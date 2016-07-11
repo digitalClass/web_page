879f888479efa3aea027b3a0bd1fc35c9109cb3e
zhyh2010 <1435589631@qq.com>
2016-07-11 23:20:11 +0800

    原始首页版本
    Signed-off-by: zhyh2010 <1435589631@qq.com>

---
+++ static/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ThinkPad User on 2016/7/11 0011.
+ */
+
+//function js_list_math_mouseover(){
+//    //alert('hello')
+//    document.getElementsByClassName('listbox_content').display = True;
+//}
+
+//$(function(){
+//    $('div#libox li').mouseover(function(){alert($(this).find('span').html());});
+//});
+
+//function js_list_math_mouseover(){
+//    x = document.getElementsByClassName("listbox_content");
+//    x[0].style.display = "inline";
+//}
+//
+//function js_list_math_mouseover_pass(){
+//    x = document.getElementsByClassName("listbox_content");
+//    x[0].style.display = "none";
+//}
+
+
