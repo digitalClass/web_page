452fb278f7bea1dd415fd1fc86b0c84322a7de9e
zhyh2010 <1435589631@qq.com>
2016-07-15 22:34:29 +0800

    修正login 和 register 的界面显示效果
    Signed-off-by: zhyh2010 <1435589631@qq.com>

---
+++ static/js/field.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ThinkPad User on 2016/7/11 0011.
+ */
+
+$(function(){
+    var text1 = $(".errorlist li").first().text()
+    $(".errorlist").html(text1);
+    $(".errorlist").css("color", "#3c763d")
+})
+
