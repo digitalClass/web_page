a00030c3dc7235713f3d46a9b5225c9b6962efe1
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-08-30 20:53:30 +0800

    首页增加翻页按钮

--- static/js/index.js
+++ static/js/index.js
@@ -23,6 +23,86 @@ $(function(){
                     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
 
+	$(".each_page").click(function(e){
+			var csrftoken = getCookie("csrftoken");
+			$.ajaxSetup({beforeSend: function(xhr, settings){
+					if (!csrfSafeMethod(settings.type) && !this.crossDomain){
+							xhr.setRequestHeader("X-CSRFToken", csrftoken);
+					}
+			}});
+
+			var page_id = -1;
+            var curpage_c = $(".js_curpage");
+			console.log(curpage_c)
+            var curpage = parseInt($(curpage_c).text(), 10);
+			$("li:contains('"+$(curpage_c).text()+"')").removeClass("active");
+			console.log(curpage);
+			var page_id_c = e.currentTarget.children[0].textContent;
+			if (page_id_c === "\xAB"){
+				page_id = curpage - 1;
+			}
+			else if (page_id_c === "\xBB"){
+				page_id = curpage + 1;
+			}
+			else {
+				page_id = parseInt(page_id_c, 10)
+			}
+			console.log('page_id:'+page_id);
+            var totalpage_c = $(".js_totalpage");
+            var totalpage = parseInt($(totalpage_c).text(), 10);
+			console.log(totalpage);
+			if (page_id > 1){
+				$("li:contains('\xAB')").removeClass("disabled");
+			}
+			if (page_id === totalpage){
+				$("li:contains('\xBB')").addClass("disabled");
+			}
+			if (page_id < 4){
+				$("li:contains('\xBB')").removeClass("disabled");
+			}
+			if (page_id === 1){
+				$("li:contains('\xAB')").addClass("disabled");
+			}
+            if ( page_id <= 0 || page_id > totalpage){
+                return;
+            }
+			 $.ajax({
+                url: '/page_change/',
+                type: 'post',
+                dataType: 'json',
+                data: {'page_id': page_id}
+            }).done(function (oResult){
+                if(oResult.code != 0){
+                    return alert(oResult.msg || '请求失败，请稍后重试');
+                }
+
+                var oListDv = $(".js-class_list_main");
+                oListDv.html('');
+
+                // 渲染新的评论
+                for (var i = 0; i < oResult.item_count; i++) {
+                    var sHtml = [
+                        '<tr>',
+                        '<th>', oResult.course[i].id, '</th>' +
+                        '                        <th><a href=/course/', oResult.course[i].id, '>', oResult.course[i].title, '</a></th>' +
+                        '                      <th>', oResult.course[i].create_time, '</th>' +
+                        '                      <th>', oResult.course[i].teacher_name, '</th>' +
+                        '                    </tr>'
+                    ].join('');
+                    $(oListDv).append(sHtml);
+                }
+
+                $(curpage_c).text(page_id);
+				$("li:contains('"+page_id.toString()+"')").addClass("active");
+            }).fail(function (oResult){
+                alert(oResult.msg || '请求失败，请稍后重试');
+            }). always(function () {
+
+            });
+
+
+
+		});
      $(".js-carousel").click(function(e){
             var csrftoken = getCookie('csrftoken');
             $.ajaxSetup({
