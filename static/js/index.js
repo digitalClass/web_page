5829591f86a3e09405d2da03c96e38060c119c3e
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-09-11 20:05:29 +0800

    fix bug

--- static/js/index.js
+++ static/js/index.js
@@ -83,7 +83,7 @@ $(function(){
                 for (var i = 0; i < oResult.item_count; i++) {
                     var sHtml = [
                         '<tr>',
-                        '<th>', oResult.course[i].id, '</th>' +
+                        '<th>', oResult.course[i].course_id, '</th>' +
                         '                        <th><a href=/course/', oResult.course[i].id, '>', oResult.course[i].title, '</a></th>' +
                         '                      <th>', oResult.course[i].create_time, '</th>' +
                         '                      <th>', oResult.course[i].teacher_name, '</th>' +
